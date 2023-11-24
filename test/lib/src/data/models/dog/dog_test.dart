96f72f89a18028890422898fb7ddbf9b37d39054
Gabriele <mgabriele00@gmail.com>
2023-11-24 17:55:55 +0100

    updated test after mock data change

--- test/lib/src/data/models/dog/dog_test.dart
+++ test/lib/src/data/models/dog/dog_test.dart
@@ -7,7 +7,7 @@ void main() {
   test('Should convert a message response into List<Dog>', () {
 
     const expectedResult = <Dog>[
-      Dog(breed: 'breed1', subBreeds: ['subBreed1', 'subBreed2']),
+      Dog(breed: 'breed1', subBreeds: ['subBreed1', 'subBreed2', 'subBreed3']),
       Dog(breed: 'breed2', subBreeds: []),
       Dog(breed: 'breed3', subBreeds: ['subBreed1'])
     ];
